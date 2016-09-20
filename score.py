4b2a7ad1f2c15feb4bd8c2d69c7df3725eef6d2a
Christina England <cengland@ltu.edu>
2016-09-20 09:42:52 -0400

    Update score.py

--- score.py
+++ score.py
@@ -22,12 +22,12 @@ def score_round(score_A, score_B, tricks_A, tricks_B)
         if score_A >= 10:
             score_game(score_A, score_B)
     else if tricks_B >=3:
-        if call_B = True:
-            score_B = score_B + 1 #Team B called trump, won round
+            if call_B = True:
+                score_B = score_B + 1 #Team B called trump, won round
         else:
             score_B = score_B + 2 #Team B euchred Team A
-        if score_B >= 10:
-            score_game(score_A, score_B)
+            if score_B >= 10:
+                score_game(score_A, score_B)
 
 #notify who the winner is
 def score_game (score_A, score_B)
