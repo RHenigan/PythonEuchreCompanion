29b14b181c14d5d3497dfd47bcfb4ab5fbfbe7b0
RHeniz <rhenigan@ltu.edu>
2016-10-03 16:45:53 -0400

    Update cardClass.py
    cardClass should be finished

--- cardClass.py
+++ cardClass.py
@@ -1,47 +1,46 @@
 #/usr/bin/python3
-#TEST COMMENT FOR COMMIT
+
 import binascii
 import sys
 
 import Adafruit_PN532 as PN532
+#heart, spade, diamond, club
+suitsArray = [unichr(0), unichr(0), unichr(1), unichr(2), unichr(3)]
 
-suitsArray = ["", "hearts", "spades", "diamonds", "clubs"]
-cardsList = []
-
+#pins
 CS = 'P8_7'
 MOSI = 'P8_8'
 MISO = 'P8_9'
 SCLK = 'P8_10'
 
+#initialize reader
 pn532 = PN532.PN532(cs=CS, sclk=SCLK, mosi=MOSI, miso=MISO)
 
+#start reader
 pn532.begin()
 
+#get version
 ic, ver, rev, support = pn532.get_firmware_version()
 print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
 
+#configure
 pn532.SAM_configuration()
 
-def checkDups(uid):
-    if uid in cardsList:
-        print("card exists")
-        return True
-    else:
-        print("new card read")
-        cardsList.append(uid)
-        return False
 
+#get cards unique ID
 def getUID():
     print("Reading UID.....")
     while True:
         uid = pn532.read_passive_target()
         
         if uid is not None:
-            if checkDups(uid) is False:
-                print("UID Read succesful")
-                return uid
+            print("UID Read succesful")
+            return uid
 
+#get cards data
 def getData(uid):
+    #default card val to 0
+    cardVal = '0'
     print("Reading Data.....")
     while True:
         if not pn532.mifare_classic_authenticate_block(uid, 4, PN532.MIFARE_CMD_AUTH_B, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
@@ -50,42 +49,33 @@ def getData(uid):
         data = pn532.mifare_classic_read_block(4)
 
         if data is not None:
+            #return data as an array data[2] = val data[3] = suit
             print("data read successful")
             return data
+        else:
+            #else try again
+            uid = getUID()
     
 class Cards():
-    def __init__(self, uid, val, suit):
-        self.uid = uid
-        self.suit = suit
-        self.val = val  
-    def retUID():
-        return self.uid
-
-
-def main():
-    print("Player 1 Hand one card at a time")
-
-#P1Hand = [Cards() for i in range(5)]
-
-    P1Hand = []
-
-    for i in range(5):
-        uid = getUID()
-        data = getData(uid)
-        x = Cards(uid, data[2], suitsArray[data[3]])
-        P1Hand.append(x)
-        print("Card {0} Recorded".format(i + 1))
-            #print("P1 Card {0}: {1} of {2}".format(i+1, P1Hand[i].val, P1Hand[i].suit))
-            #raw_input("Enter for next card")
-        
-    for i in range(5):
-        print("P1 Card {0}: {1} of {2}".format(i+1, P1Hand[i].val, P1Hand[i].suit))
-
-    cardsList[:] = []
-
-    #print("Player 1 play a card")
-    #P1T1 = Cards()
-    #print("Player 1 played: {0} of {1} this turn".format(P1T1.val, P1Hand[i].suit))
-
-
-main()
+    #initialize a card
+    def __init__(self):
+        #get uid
+        self.uid = getUID()
+        #get data
+        data = getData(self.uid)
+        #get suit
+        self.suit = suitsArray[data[3]]
+        #get card val
+        self.val = '0'
+        if data[2] is 9:
+            self.val = '9'
+        if data[2] is 10:
+            self.val = 'T'
+        if data[2] is 11:
+            self.val = 'J'
+        if data[2] is 12:
+            self.val = 'Q'
+        if data[2] is 13:
+            self.val = 'K'
+        if data[2] is 14:
+            self.val = 'A'
